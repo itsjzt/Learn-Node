31d92507fe9866ac7bf126fdd020010ec39fe124
itsjzt <saurabh.19@outlook.com>
2017-12-01 15:27:01 +0530

    handle image uploads

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -1,5 +1,16 @@
 const mongoose = require('mongoose')
 const Store = mongoose.model('Store')
+const multer = require('multer')
+const jimp = require('jimp')
+const uuid = require('uuid')
+
+const multerOptions = {
+  storage: multer.memoryStorage(),
+  fileFilter(req, file, next) {
+    const isPhoto = file.mimetype.startsWith('image/')
+    isPhoto ? next(null, true) : next({ message: "this file type isn't allowed" }, false)
+  }
+}
 
 exports.homePage = (req, res) => {
   res.render('index')
@@ -7,6 +18,24 @@ exports.homePage = (req, res) => {
 
 exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' })
+}
+
+exports.upload = multer(multerOptions).single('photo')
+
+exports.resize = async (req, res, next) => {
+  // check if there is any file to resize
+  if (!req.file) {
+    next();
+    return;
+  }
+  const extension = req.file.mimetype.split('/')[1]
+  req.body.photo = `${uuid.v4()}.${extension}`
+  // now we resize
+  const photo = await jimp.read(req.file.buffer)
+  await photo.resize(800, jimp.AUTO)
+  await photo.write(`./public/uploads/${req.body.photo}`) 
+  // once file is saved, keep going
+  next()
 }
 
 exports.createStore = async (req, res) => {
